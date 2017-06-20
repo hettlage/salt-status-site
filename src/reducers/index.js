2d9ca962ecf9cfc6092eee07d958f46956b6bfdb
Christian Hettlage <hettlage@saao.ac.za>
2017-06-20 23:22:04 +0200

    Added user reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,40 @@
+// @Flow
+
+import A from '../constants/action-types';
+
+const initialUser = {
+    firstName: '',
+    lastName: '',
+    authToken: '',
+    isLoggedIn: false,
+    isError: false,
+    isFetching: false
+};
+
+export const user = (state=initialUser, action={}) => {
+    switch (action.type) {
+    case A.LOGIN_USER_REQUEST:
+        return {
+            ...state,
+            isError: false,
+            isFetching: true
+        };
+    case A.LOGIN_USER_SUCCESS:
+        return {
+                ...success,
+            firstName: action.firstName,
+            lastName: action.lastName,
+            authToken: action.authToken,
+            isLoggedIn: true,
+            isFetching: false
+        };
+    case A.LOGIN_USER_ERROR:
+        return {
+                ...success,
+            isError: true,
+            isFetching: false
+        };
+    default:
+        return state;
+    }
+};
